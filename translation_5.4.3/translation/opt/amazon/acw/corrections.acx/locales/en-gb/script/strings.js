14acb37953f33eaa4be1c40046e615641dccd404
Aryeh Beitz <aryehbeitz@gmail.com>
2014-06-15 02:02:25 +0300

    Translated

--- translation_5.4.3/translation/opt/amazon/acw/corrections.acx/locales/en-gb/script/strings.js
+++ translation_5.4.3/translation/opt/amazon/acw/corrections.acx/locales/en-gb/script/strings.js
@@ -1,14 +1,14 @@
-A customer support specialist will look into this error. You will be able to view the status on your profile page on kindle.amazon.com the next time this device synchronises with Amazon.	
-A customer support specialist will look into this error.	
-Add Optional Comments.	
+A customer support specialist will look into this error. You will be able to view the status on your profile page on kindle.amazon.com the next time this device synchronises with Amazon.	מומחה תמיכת לקוחות יבדו' שגיאה זו. תהיה מסוגל לראות מה המצב בדף הפרופיל שלך ב kindle.amazon.com בפעם הבאה שהמכשיר זה מסנכרן עם אמזון.
+A customer support specialist will look into this error.	מומחה תמיכת לקוחות יבדוק שגיאה זו.
+Add Optional Comments.	הוסף תגובה (לא חובה).
 Cancel	בטל
 Close	סגור
-Formatting	
-Image	
-Other	
-Report Content Error	
-Report your selection as having an error.	
+Formatting	עיצוב
+Image	תמונה
+Other	אחר
+Report Content Error	דווח שגיעה בתוכן
+Report your selection as having an error.	דווח על הסימון שלך, שיש בו שגיאה.
 Submit	שלח
-Thank You	
-The reporting of content errors is not possible with this content.	
-Typo	
+Thank You	תודה רבה
+The reporting of content errors is not possible with this content.	הדיווח של טעויות תוכן אינו אפשרי עם תוכן זה.
+Typo	שגיעת כתיב
