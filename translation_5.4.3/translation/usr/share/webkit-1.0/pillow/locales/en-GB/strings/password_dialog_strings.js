db9e429e9cdd18f2d15ec2ca80666a9ae12335fe
Aryeh Beitz <aryehbeitz@gmail.com>
2014-06-14 23:42:12 +0300

    Translated

--- translation_5.4.3/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/password_dialog_strings.js
+++ translation_5.4.3/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/password_dialog_strings.js
@@ -1,11 +1,11 @@
-Enter Passcode	
-Forgotten Passcode?	
-If you have forgotten your passcode, please contact Kindle Customer Service at www.kindle.com/support to receive help resetting your device.	
-Incorrect Passcode	
-OK	
-Passcode Required	
-Passcode hint: 	
-The passcode you entered is incorrect. Please try again.	
-This device has been disabled remotely.	
-Your Kindle will reset to factory defaults after 2 more failed attempts.	
-Your Kindle will reset to factory defaults after the next failed attempt.	
+Enter Passcode	הזן את הסיסמה
+Forgotten Passcode?	שכחך שיסמה?
+If you have forgotten your passcode, please contact Kindle Customer Service at www.kindle.com/support to receive help resetting your device.	אם שכחת את קוד הגישה שלך, אנא פנה לשירות לקוחות קינדל ב www.kindle.co/suport כדי לקבל עזרה לאפס המכשיר שלך.
+Incorrect Passcode	סיסמה שגויה
+OK	אישור
+Passcode Required	נדרש סיסמה
+Passcode hint: 	רמז לסיסמה:
+The passcode you entered is incorrect. Please try again.	הסיסמה שהזנת שגויה. אנא נסה שוב.
+This device has been disabled remotely.	התקן זה הושבת מרחוק.
+Your Kindle will reset to factory defaults after 2 more failed attempts.	קינדל שלך יאופס לברירות מחדל של היצרן לאחר 2 ניסיונות נוספים שייכשלו.
+Your Kindle will reset to factory defaults after the next failed attempt.	קינדל שלך יאופס לברירות מחדל של היצרן לאחר הניסיון הכושל הבא.
