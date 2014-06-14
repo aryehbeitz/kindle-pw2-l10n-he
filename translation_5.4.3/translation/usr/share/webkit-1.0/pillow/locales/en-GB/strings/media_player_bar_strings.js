02772ec4bbd72b585f4d32e6e43815d619995320
Aryeh Beitz <aryehbeitz@gmail.com>
2014-06-15 00:21:36 +0300

    Translated

--- translation_5.4.3/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/media_player_bar_strings.js
+++ translation_5.4.3/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/media_player_bar_strings.js
@@ -1,5 +1,5 @@
-0 {right} 0 0	
+0 {right} 0 0	0 {right} 0 0
 Off	כבה
-Track {index,number,integer} of {count,number,integer}	
-Unknown Artists	
-{width}px	
+Track {index,number,integer} of {count,number,integer}	רצועה {index,number,integer} מתוך {count,number,integer}
+Unknown Artists	אמן לא ידוע
+{width}px	{width}px
