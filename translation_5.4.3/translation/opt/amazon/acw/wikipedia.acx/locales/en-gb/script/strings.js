2ee3f149ca11546c5590bca120cde89db6e92098
Aryeh Beitz <aryehbeitz@gmail.com>
2014-06-15 02:45:10 +0300

    Translated

--- translation_5.4.3/translation/opt/amazon/acw/wikipedia.acx/locales/en-gb/script/strings.js
+++ translation_5.4.3/translation/opt/amazon/acw/wikipedia.acx/locales/en-gb/script/strings.js
@@ -1,16 +1,16 @@
-Aeroplane Mode must be turned off to use this feature.	
-An error occurred. Please try again.	
-Launch Wikipedia	
-No Wikipedia results were found for your selection.	
-No network detected. Please check your connection and try again.	
-No text was selected.	
-Open Wikipedia ▶	
-Search Google ▶	
-Searching Wikipedia...	
-Turn Off Aeroplane Mode	
+Aeroplane Mode must be turned off to use this feature.  מצב טיסה חייב להיות כבוי כדי להשתמש בתכונה זו.
+An error occurred. Please try again.	An error occurred. Please try again.	אירעה שגיאה. אנא נסה שוב.
+Launch Wikipedia	הפעל ויקיפידיה
+No Wikipedia results were found for your selection.	לא נמצרו תוצאות ויקיפדיה עבור הבחירה שלך.
+No network detected. Please check your connection and try again.	רשת לא זוהתה. אנא בדוק את החיבור שלך ונסה שוב.
+No text was selected.	אין טקטס נבחר.
+Open Wikipedia ▶	פתח ויקיפידיה ▶
+Search Google ▶	פתח Google ▶
+Searching Wikipedia...	מחפש בויקיפידיה...
+Turn Off Aeroplane Mode	כבה מצב טיסה
 Turn On Wireless	הפעל אלחוטי
-Unable to contact the server. Please try again later.	
-Waiting for connection...	
-Wikipedia lookups are not available when using FreeTime.	
-Wikipedia	
-Wireless must be turned on to use this feature.	
+Unable to contact the server. Please try again later.	לא ניתן ליצור קשר עם השרת. אנא נסה שוב מאוחר יותר.
+Waiting for connection...	ממתין לחיבור...
+Wikipedia lookups are not available when using FreeTime.	לא ניתן לחפש בויקיפידיה במצב פריי-טיים.
+Wikipedia	ויקיפידיה
+Wireless must be turned on to use this feature.	אלחוטי חייב להיות מופעל כדי להשתמש בתכונה זו.
