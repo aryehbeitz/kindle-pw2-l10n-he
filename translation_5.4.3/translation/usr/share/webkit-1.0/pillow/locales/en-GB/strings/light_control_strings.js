b00df3c95beffa05c6e19252057825cdae925409
Aryeh Beitz <aryehbeitz@gmail.com>
2014-06-14 23:31:36 +0300

    Translated

--- translation_5.4.3/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/light_control_strings.js
+++ translation_5.4.3/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/light_control_strings.js
@@ -1,13 +1,13 @@
 &#9654	
 Advanced	
 Auto Brightness	
-In brightly lit rooms, use a <u>high</u> setting.	
-Max.	
+In brightly lit rooms, use a <u>high</u> setting.	השתמש ההגדרה <u>גבוהה</u> לחדרים חשוכים
+Max.	מקסימום
 Off	כבה
-On	פתח
+On	הפעל
 Recommended for Bright Rooms	
 Recommended for Dark Rooms	
 Recommended for Dim Rooms	
 Recommended for Overcast Conditions	
 Recommended for Sunlight	
-Use a low setting for dark rooms.	
+Use a low setting for dark rooms. השתמש בהגדרה נמוכה לחדרים מוארים.
