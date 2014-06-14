3aa208b604122a70e6ac815183e48e8e35925ef9
Aryeh Beitz <aryehbeitz@gmail.com>
2014-06-15 02:58:34 +0300

    Translated

--- translation_5.4.3/translation/opt/amazon/acw/xray.acx/locales/en-gb/strings.js
+++ translation_5.4.3/translation/opt/amazon/acw/xray.acx/locales/en-gb/strings.js
@@ -1,51 +1,51 @@
-(cont.)	
+(cont.)	המשך...
 All	הכל
-Artists	
-Book	
+Artists	אמנים
+Book	ספר
 Chapter	פרק
-Creatures	
-Current Chapter	
-Current Page	
-Dates	
-Entire Book	
-Equations	
-Events	
-Full Wikipedia Article	
+Creatures	יצורים
+Current Chapter	פרק הנוכחי
+Current Page	עמוד הנוכחי
+Dates	תאריכים
+Entire Book	כל הספר
+Equations	משוואות
+Events	אירועים
+Full Wikipedia Article	מאמר ויקיפדיה מלא
 Highlights	סימונים
-Holidays	
-Hotels	
-Ingredients	
-Location ${location}	
-Locations ${start} - ${end}	
-Monsters	
-More on Shelfari	
+Holidays	חגים
+Hotels	מלונות
+Ingredients	מרכיבים
+Location ${location}	מיקום ${location}
+Locations ${start} - ${end}	מקומות ${start} - ${end}
+Monsters	מפלצות
+More on Shelfari	עוד ב Shelfari
 More	עוד
-Organisations	
-Page ${page}	
-Page ${start} - ${end}	
-Page	
-People	
-Places	
-Potions	
-Quotes	
-Recipes	
-Restaurants	
-See more about this book.	
-Showing ${range} of ${total}	
-Spells	
-Subject to a <span class="license">Creative Commons Licence</span>	
-Terms	
-Theorems	
-This book does not contain X-Ray concepts.	
-This chapter does not contain X-Ray concepts.	
-This page does not contain X-Ray concepts.	
-Works	
-X-Ray concepts are not yet available for this book. See additional book information.	
-X-Ray is not available for this book	
-X-Ray	
-en-gb	
-http://creativecommons.org/licenses/by-sa/3.0/	
-loc.${location}	
-loc.${start} - ${end}	
-p.${page}	
-p.${start} - ${end}	
+Organisations	ארגונים
+Page ${page}	עמוד ${page}
+Page ${start} - ${end}	עמוד ${start} - ${end}
+Page	עמוד
+People	אנשים
+Places	מקומות
+Potions	השיקויים
+Quotes	ציטוטים
+Recipes	מתכונים
+Restaurants	מסעדות
+See more about this book.	ראה עוד על הספר הזה.
+Showing ${range} of ${total}	מראה ${range} מתוך ${total}
+Spells	לחשים
+Subject to a <span class="license">Creative Commons Licence</span>	בכפוף ל <span class="license">Creative Commons Licence</span>
+Terms	תנאים
+Theorems	תיאוריות
+This book does not contain X-Ray concepts.	ספר זה אינו מכיל מושגי אקס-ריי.
+This chapter does not contain X-Ray concepts.	פרק זה אינו מכיל מושגי אקס-ריי.
+This page does not contain X-Ray concepts.	עמוד זה אינו מכיל מושגי אקס-ריי.
+Works	עבודות
+X-Ray concepts are not yet available for this book. See additional book information.	מושגי אקס-ריי עדיין אינם זמינים עבור ספר זה. ראה מידע ספר נוסף.
+X-Ray is not available for this book	אקס-ריי אינו זמין לספר הזה
+X-Ray	אקס-ריי
+en-gb	en-gb
+http://creativecommons.org/licenses/by-sa/3.0/	http://creativecommons.org/licenses/by-sa/3.0/
+loc.${location}	loc.${location}
+loc.${start} - ${end}	loc.${start} - ${end}
+p.${page}	p.${page}
+p.${start} - ${end}	p.${start} - ${end}
