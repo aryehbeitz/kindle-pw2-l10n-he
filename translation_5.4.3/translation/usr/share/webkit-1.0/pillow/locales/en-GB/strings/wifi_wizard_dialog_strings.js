8ea0f112cce12489aba95398af3408676e3c68a4
Aryeh Beitz <aryehbeitz@gmail.com>
2014-06-15 01:48:59 +0300

    Translated

--- translation_5.4.3/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/wifi_wizard_dialog_strings.js
+++ translation_5.4.3/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/wifi_wizard_dialog_strings.js
@@ -1,87 +1,87 @@
-Advanced Options	
-Advanced	
-Are you sure you want to forget the Wi-Fi network "{essid}"?	
-Available Networks	
-CA Certificate	
-Cancel	בטל#or#ביטול
-Connect	
-Connected	
-Connecting	
-Connection Failed	
-Connection Type	
-DHCP	
-DNS	
-Disconnect	
-Done	
-EAP Method	
-Enter Again	
-Enter Other Wi-Fi Network	
-Enter Wi-Fi Network	
-Enterprise	
-Error	
-Forget Network?	
-Forget	
-Hide Password	
-IP Address	
+Advanced Options	אפשרויות מתקדמות
+Advanced	מתקדם
+Are you sure you want to forget the Wi-Fi network "{essid}"?	בטוח לשכוח את רשת אלחוטי "{essid}"?
+Available Networks	רשתות זמינות
+CA Certificate	תעודת CA
+Cancel	בטל
+Connect	התחבר
+Connected	מחובר
+Connecting	מתחבר
+Connection Failed	התחברות נכשלה
+Connection Type	סוג חיבור
+DHCP	DHCP
+DNS	DNS
+Disconnect	התנתק
+Done	סיים
+EAP Method	EAP Method
+Enter Again	הקש שוב
+Enter Other Wi-Fi Network	הקש רשת אלחוטי אחר
+Enter Wi-Fi Network	הקש רשת אלחוטי
+Enterprise	Enterprise
+Error	שגיעה
+Forget Network?	ששכת רשת?
+Forget	תשכח
+Hide Password	הסתר סיסמא
+IP Address	כתובת IP
 Incorrect Password	סיסמא שגוייה
-MSCHAPv2	
-Network Name Too Long	
-Network Name	
-No Wi-Fi networks were found	
-None	
-OK	
-Other...	
-PAP	
-PEAP	
-Password Too Long	
-Password Too Short	
-Password	
-Personal	
-Phase 2 Authentication	
-Rescan	
-Router	
-Scan Complete	
-Scanning for Networks	
-Security Error	
-Security Type	
-Set Up	
-Static	
-Subnet Mask	
-TTLS	
-The security certificate cannot be located. Please verify that the name is correct and try again.	
-Try Again	
-Unable to Find Certificate	
-Unable to delete profile for Wi-Fi network "{essid}".	
-Unable to set up the Wi-Fi network "{essid}". The network information you entered is incomplete. Please check network configuration and try setting up the network again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Unsupported Network	
-Username	
-Version	
-WEP	
-WPA/WPA2	
-WPA2	
-WPS	
-Wi-Fi Error	
-Wi-Fi Login	
-Wi-Fi Networks ({numNetworks})	
-Wi-Fi Not Configured	
-Wi-Fi Password Required	
-You can set up the Wi-Fi network connection and security options for the network named below.	
-Your Kindle connected to the Wi-Fi network but could not reach the Internet. Contact your Internet Service Provider for further assistance.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}" using WPS. Please check that the WPS button was tapped.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}" using WPS. Would you like to set up this network manually?	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". --"{error}"--. Press Set Up to enter your password again or set up your network manually. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". Check that your security settings are correct and try again.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". Enterprise Wi-Fi networks are not supported on Kindle. Try connecting to another network. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". Peer-to-peer Wi-Fi networks are not supported on Kindle. Try connecting to another network. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". Please check your credentials or contact your system administrator.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". Press Set Up to enter your password again or set up your network manually. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". Tap the Home button, then connect to Wi-Fi again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". The access point rejected the connection request. Check your routerʼs MAC address filtering settings and try again.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". The network name is too long. Would you like to set up this network manually?	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". The password you entered is incorrect. Try entering your password again or setting up your network manually. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". The password you entered is longer than allowed by the Wi-Fi network. Try entering your password again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". The password you entered is missing one or more characters. Try entering your password again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". The security type you entered does not match the security type of the Wi-Fi network. Please set up the network manually again and select the correct security type. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network "{essid}". Try entering your username and password again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to the Wi-Fi network. This network is not configured. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
-Your Kindle is unable to connect to your local network "{essid}". You may need to check your local network. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	
+MSCHAPv2	MSCHAPv2
+Network Name Too Long	שם רשת ארוך מדי
+Network Name	שם רשת
+No Wi-Fi networks were found	לא נמצאו רשתות אלחוטיות
+None	ללא
+OK	אישור
+Other...	אחר...
+PAP	PAP
+PEAP	PEAP
+Password Too Long	סיסמא ארוך מדי
+Password Too Short	סיסמא קצר מדי
+Password	סיסמא
+Personal	Personal
+Phase 2 Authentication	Phase 2 Authentication
+Rescan	לסרוק מחדש
+Router	נתב
+Scan Complete	הושלת סריקה
+Scanning for Networks	סורק לרשתות
+Security Error	שגיאת אבטחה
+Security Type	סוג אבטחה
+Set Up	הגדר
+Static	Static
+Subnet Mask	Subnet Mask
+TTLS	TTLS
+The security certificate cannot be located. Please verify that the name is correct and try again.	לא ניתן לאתר תעודת האבטחה. אנא ודא שהשם הוא נכון ונסה שוב.
+Try Again	נסה שוב
+Unable to Find Certificate	לא ניתן למצוא התעודה
+Unable to delete profile for Wi-Fi network "{essid}".	לא ניתן למחוק פרופיל לשרת אלחוטי "{essid}".
+Unable to set up the Wi-Fi network "{essid}". The network information you entered is incomplete. Please check network configuration and try setting up the network again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	אין אפשרות להגדיר רשת Wi-Fi "{essid}". המידע ברשת שהזנת אינו שלם. בדוק את תצורת רשת ולנסות להקים את הרשת שוב. אם יש לך בעיות חיבור קינדל שלך ל-Wi-Fi, אתה יכול למצוא עזרה ב www.kindle.com/support.
+Unsupported Network	רשת שאינה נתמכת
+Username	שם משתמש
+Version	גרסה
+WEP	WEP
+WPA/WPA2	WPA/WPA2
+WPA2	WPA2
+WPS	WPS
+Wi-Fi Error	שגיעה באלחוטית
+Wi-Fi Login	כניסה אלחוטית
+Wi-Fi Networks ({numNetworks})	רשתות אלחוטיות ({numNetworks})
+Wi-Fi Not Configured	רשת אלחוטית לא מודגר
+Wi-Fi Password Required	נדרש סיסמה לאלחוטי
+You can set up the Wi-Fi network connection and security options for the network named below.	ניתן להגדיר את חיבור רשת אלחוטי ואפשרויות אבטחה עבור הרשת בשם למטה.
+Your Kindle connected to the Wi-Fi network but could not reach the Internet. Contact your Internet Service Provider for further assistance.	קינדל שלך מחובר לרשת Wi-Fi, אבל לא הצליח להגיע לאינטרנט. פנה לספק שירותי האינטרנט שלך לקבלת סיוע נוסף.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}" using WPS. Please check that the WPS button was tapped.	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}" דרף WPS. אנא בדוק שלחצן WPS היה טפח.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}" using WPS. Would you like to set up this network manually?	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}" באמצעות WPS. האם אתה רוצה להגדיר רשת זו באופן ידני?
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". --"{error}"--. Press Set Up to enter your password again or set up your network manually. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}" --"{error}"--. הקש הגדר כדי להזין את הסיסמה שלך שוב או התקין את רשת באופן ידני. אם יש לך בעיות חיבור קינדל שלך ל-Wi-Fi, אתה יכול למצוא עזרה ב www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". Check that your security settings are correct and try again.	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}". בדוק שהגדרות האבטחה שלך נכונות ונסה שוב.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". Enterprise Wi-Fi networks are not supported on Kindle. Try connecting to another network. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}". רשתות Enterprise Wi-Fi אינן נתמכות על הקינדל. נסה להתחבר לרשת אחרת. אם יש לך בעיות חיבור קינדל שלך ל-Wi-Fi, אפשר למצוא עזרה ב www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". Peer-to-peer Wi-Fi networks are not supported on Kindle. Try connecting to another network. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support. הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}" Peer-to-peer. רשתות Wi-Fi אינן נתמכות על הקינדל. נסה להתחבר לרשת אחרת. אם יש לך בעיות חיבור קינדל שלך ל-Wi-Fi, ניתן למצוא עזרה ב www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". Please check your credentials or contact your system administrator.	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}". אנא בדוק האישורים שלך או פנה למנהל המערכת שלך.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". Press Set Up to enter your password again or set up your network manually. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}". הקש הגדר כדי להזין את הסיסמה שלך שוב או התקין את רשת באופן ידני. אם יש לך בעיות חיבור קינדל שלך ל-Wi-Fi, אתה יכול למצוא עזרה ב www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". Tap the Home button, then connect to Wi-Fi again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}". הקש על כפתור הבית, ולאחר מכן להתחבר לאינטרנט אלחוטי שוב. אם יש לך בעיות חיבור קינדל שלך ל-Wi-Fi, אתה יכול למצוא עזרה ב www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". The access point rejected the connection request. Check your routerʼs MAC address filtering settings and try again.	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}". נקודת הגישה דחתה את בקשת החיבור. בדקו בהגדרות הנתב בסינון כתובות MAC ונסה שוב.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". The network name is too long. Would you like to set up this network manually?	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}". שם הרשת ארוך מדי. האם להגדיר רשת זו באופן ידני?
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". The password you entered is incorrect. Try entering your password again or setting up your network manually. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}". הסיסמה שהזנת שגויה. נסה להזין שוב את הסיסמה או הגדיר הרשת שלך באופן ידני. אם יש לך בעיות חיבור בקינדל שלך ל-Wi-Fi, אפשר למצוא עזרה ב www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". The password you entered is longer than allowed by the Wi-Fi network. Try entering your password again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}". הסיסמה שהזנת ארוכה יותר מהמותר על ידי רשת Wi-Fi. נסה להזין שוב את הסיסמה. אם יש לך בעיות חיבור קינדל שלך ל-Wi-Fi, אתה יכול למצוא עזרה ב www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". The password you entered is missing one or more characters. Try entering your password again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}". הסיסמה שהזנת חסרה תו אחד או יותר. נסה להזין שוב את הסיסמה. אם יש לך בעיות בחיבור הקינדל שלך ל-Wi-Fi, אפשר למצוא עזרה ב www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". The security type you entered does not match the security type of the Wi-Fi network. Please set up the network manually again and select the correct security type. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}". סוג האבטחה שהזנת אינו תואם את סוג האבטחה של רשת Wi-Fi. אנא הגדר את הרשת באופן ידני שוב ובחר את סוג האבטחה הנכון. אם יש לך בעיות חיבור קינדל שלך ל-Wi-Fi, אפשר למצוא עזרה ב www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network "{essid}". Try entering your username and password again. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	הקינדל שלך הוא לא הצליח להתחבר לרשת Wi-Fi "{essid}". נסה להזין את שם המשתמש והסיסמה שלך שוב. אם יש לך בעיות חיבור קינדל שלך ל-Wi-Fi, אפשר למצוא עזרה ב www.kindle.com/support.
+Your Kindle is unable to connect to the Wi-Fi network. This network is not configured. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	הקינדל שלך הוא לא הצליח להתחבר לרשת אלחוטי. רשת זו אינה מוגדרת. אם יש לך בעיות חיבור קינדל שלך ל-Wi-Fi, אפשר למצוא עזרה ב www.kindle.com/support.
+Your Kindle is unable to connect to your local network "{essid}". You may need to check your local network. If you have issues connecting your Kindle to Wi-Fi, you can find help at www.kindle.com/support.	הקינדל שלך הוא לא הצליח להתחבר לרשת אלחוטי מקומי. ייתכן שצריך לבדוק את הרשת המקומית שלך. אם יש לך בעיות חיבור קינדל שלך ל-Wi-Fi, אפשר למצוא עזרה ב www.kindle.com/support.
