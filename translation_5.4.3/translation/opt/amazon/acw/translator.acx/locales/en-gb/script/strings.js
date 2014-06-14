3c1114f55b342a230fbc17b730fe6c58d2504fbb
Aryeh Beitz <aryehbeitz@gmail.com>
2014-06-15 02:35:14 +0300

    Translated

--- translation_5.4.3/translation/opt/amazon/acw/translator.acx/locales/en-gb/script/strings.js
+++ translation_5.4.3/translation/opt/amazon/acw/translator.acx/locales/en-gb/script/strings.js
@@ -1,41 +1,41 @@
-About Translation	
-Aeroplane Mode must be turned off to use this feature.	
-An error occurred. Please try again.	
-Back to Translation	
-Chinese Simplified	
-Chinese Traditional	
-Czech	
-Danish	
-Detecting...	
-Dutch	
-English	
-Finnish	
-French	
-From:	
-German	
-Greek	
-Hebrew	
-Hindi	
-Italian	
-Japanese	
-Korean	
-Latin	
-No network detected. Please check your connection and try again.	
-No text was selected for translation.	
-Norwegian	
-Polish	
-Portuguese	
-Russian	
-Slovak	
-Spanish	
-To:	
-Translating your selection...	
-Translation	
-Turkish	
-Turn Off Aeroplane Mode	
+About Translation אודות התרגום
+Aeroplane Mode must be turned off to use this feature.  מצב טיסה חייב להיות כבוי כדי להשתמש בתכונה זו.
+An error occurred. Please try again.	אירעה שגיאה. אנא נסה שוב.
+Back to Translation	חזור לתרגום
+Chinese Simplified	סיני פשוטה
+Chinese Traditional	סיני מסורתי
+Czech	צ'כית
+Danish	דני
+Detecting...	מזהה...
+Dutch	הולנדי
+English	אנגלית
+Finnish	פיני
+French	צרפתי
+From:	מאת:
+German	גרמני
+Greek	יווני
+Hebrew	עברית
+Hindi	הודי
+Italian	איטלקי
+Japanese	יפנית
+Korean	קוריאני
+Latin	לטיני
+No network detected. Please check your connection and try again.	רשת לא זוהתה. אנא בדוק את החיבור שלך ונסה שוב.
+No text was selected for translation.	אין טקסט נבחר לתרגום.
+Norwegian	נורבגי
+Polish	פולני
+Portuguese	פורטוגזית
+Russian	רוסי
+Slovak	סלובקי
+Spanish	ספרדית
+To:	ל:
+Translating your selection...	מתרגם בחירתך...
+Translation	תרגום
+Turkish	תורכי
+Turn Off Aeroplane Mode	בטל מצב טיסה
 Turn On Wireless	הפעל אלחוטי
-Ukrainian	
-Unable to contact the server. Please try again later.	
-Unknown	
-Waiting for connection...	
-Wireless must be turned on to use this feature.	
+Ukrainian	אוקראיני
+Unable to contact the server. Please try again later.	לא ניתן ליצור קשר עם השרת. אנא נסה שוב מאוחר יותר.
+Unknown	לא ידוע
+Waiting for connection...	ממתין לחיבור
+Wireless must be turned on to use this feature.	אלחוטי חייב להיות מופעל כדי להשתמש בתכונה זו.
