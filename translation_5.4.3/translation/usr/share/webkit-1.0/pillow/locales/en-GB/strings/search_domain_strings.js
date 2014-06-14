aa876b6dd46bd9b0a5b2c97a7ad4716c30bb7f2b
Aryeh Beitz <aryehbeitz@gmail.com>
2014-06-15 00:24:03 +0300

    Translated

--- translation_5.4.3/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/search_domain_strings.js
+++ translation_5.4.3/translation/usr/share/webkit-1.0/pillow/locales/en-GB/strings/search_domain_strings.js
@@ -1,11 +1,11 @@
-100pt	
-All Text	
-Baidu Baike	
-Baidu	
-Dictionary	
-Douban	
-Google	
-Kindle Store	
-My Items	
-Web Address	
-Wikipedia	
+100pt	100pt
+All Text	כל טקסט
+Baidu Baike	Baidu Baike
+Baidu	Baidu
+Dictionary	מילון
+Douban	Douban
+Google	Google
+Kindle Store	חנות קינדל
+My Items	הפריטים שלי
+Web Address	כתובת אינטרנט
+Wikipedia	ויקיפידיה
