a4e98848595a452596f20c48c329595a8a66a83f
Aryeh Beitz <aryehbeitz@gmail.com>
2014-06-15 03:12:19 +0300

    Translated

--- translation_5.4.3/translation/opt/var/local/mesquite/adviewer/locales/en-GB/scripts/resources.js
+++ translation_5.4.3/translation/opt/var/local/mesquite/adviewer/locales/en-GB/scripts/resources.js
@@ -1,20 +1,20 @@
 Close	סגור
-Details for this advert are no longer available. Turn on wireless to receive the latest content.	
-Details for this advert are no longer available.	
-Device Not Registered	
-Error	
-Offer Expired	
-Order Cancelled	
-Parental Controls are enabled on your Kindle. Purchases are not allowed.	
-Please connect wirelessly to download the latest Special Offers. New offers will display automatically when available.	
-Please register your Kindle to receive the latest Special Offers. New offers will display automatically when available.	
-Purchase Limit Reached	
-Purchased by Accident? Cancel this Order	
-Purchasing Locked	
-Special Offer	
-Thank you for your order	
-To complete your AmazonLocal order, connect to a wireless network within the next {purchaseDaysRemaining} days. We will send a voucher to the Home screen and a notification to your e-mail account once your order has processed. The voucher will contain instructions on how to redeem this offer.	
-View All Special Offers	
+Details for this advert are no longer available. Turn on wireless to receive the latest content.	פרטים למודעה זו אינם זמינים עוד. הפעל אלחוטי כדי לקבל את התוכן העדכני ביותר.
+Details for this advert are no longer available.	פרטים למודעה זו אינם זמינים עוד.
+Device Not Registered	מכשיר לא רשום
+Error	שגיעה
+Offer Expired	הצעה פג תוקף
+Order Cancelled	הזמנה בוטלה
+Parental Controls are enabled on your Kindle. Purchases are not allowed.	בקרת הורים מופעלת על קינדל שלך. רכישות לא מורשות.
+Please connect wirelessly to download the latest Special Offers. New offers will display automatically when available.	נא להתחבר באופן אלחוטי כדי להוריד את העדכונים האחרונים של ההצעות מיוחדות. הצעות חדשות תציגו באופן אוטומטי כאשר תהיו זמינות.
+Please register your Kindle to receive the latest Special Offers. New offers will display automatically when available.	נא הרשם הקינדל שלך כדי להוריד את העדכונים האחרונים של ההצעות מיוחדות. הצעות חדשות תציגו באופן אוטומטי כאשר תהיו זמינות.
+Purchase Limit Reached	תקרת רכישות הגיע
+Purchased by Accident? Cancel this Order	נרכש בטעות? בטל הזמנה זו
+Purchasing Locked	רכישות נעול
+Special Offer	הצעה מיוחדת
+Thank you for your order	תודה על ההזמנה שלך
+To complete your AmazonLocal order, connect to a wireless network within the next {purchaseDaysRemaining} days. We will send a voucher to the Home screen and a notification to your e-mail account once your order has processed. The voucher will contain instructions on how to redeem this offer.	כדי להשלים את הזמנת AmazonLocal שלך, התחבר לרשת אלחוטית בתוך {purchaseDaysRemaining} ימים. אנו נשלח שובר למסך הבית והודעה לחשבון הדואר האלקטרוני שלך ברגע שהזמנתך מעובד.השובר יכיל הוראות כיצד לממש את המבצע הזה.
+View All Special Offers	צפה בכל ההצעות המיוחדות
 View Special Offers	הצד פרסומות
 We are unable to process your request. Please try again later.	
 You already have {count} order(s) pending for this special offer.	
