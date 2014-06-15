357c7b216736c309a1b55fb0482a0493460ebe1a
Aryeh Beitz <aryehbeitz@gmail.com>
2014-06-15 03:13:25 +0300

    Translation

--- translation_5.4.3/translation/opt/var/local/mesquite/adviewer/locales/en-GB/scripts/resources.js
+++ translation_5.4.3/translation/opt/var/local/mesquite/adviewer/locales/en-GB/scripts/resources.js
@@ -15,7 +15,7 @@ Special Offer	הצעה מיוחדת
 Thank you for your order	תודה על ההזמנה שלך
 To complete your AmazonLocal order, connect to a wireless network within the next {purchaseDaysRemaining} days. We will send a voucher to the Home screen and a notification to your e-mail account once your order has processed. The voucher will contain instructions on how to redeem this offer.	כדי להשלים את הזמנת AmazonLocal שלך, התחבר לרשת אלחוטית בתוך {purchaseDaysRemaining} ימים. אנו נשלח שובר למסך הבית והודעה לחשבון הדואר האלקטרוני שלך ברגע שהזמנתך מעובד.השובר יכיל הוראות כיצד לממש את המבצע הזה.
 View All Special Offers	צפה בכל ההצעות המיוחדות
-View Special Offers	הצד פרסומות
+View Special Offers	הצד הצעות מיוחדות
 We are unable to process your request. Please try again later.	
 You already have {count} order(s) pending for this special offer.	
 You have other orders pending. Click here to view	
