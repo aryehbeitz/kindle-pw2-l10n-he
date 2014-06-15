ed01c0d2222ecdd9d86be6786a96240ea07428b2
Aryeh Beitz <aryehbeitz@gmail.com>
2014-06-15 03:21:40 +0300

    Translated

--- translation_5.4.3/translation/opt/var/local/mesquite/adviewer/locales/en-GB/scripts/resources.js
+++ translation_5.4.3/translation/opt/var/local/mesquite/adviewer/locales/en-GB/scripts/resources.js
@@ -16,13 +16,13 @@ Thank you for your order	תודה על ההזמנה שלך
 To complete your AmazonLocal order, connect to a wireless network within the next {purchaseDaysRemaining} days. We will send a voucher to the Home screen and a notification to your e-mail account once your order has processed. The voucher will contain instructions on how to redeem this offer.	כדי להשלים את הזמנת AmazonLocal שלך, התחבר לרשת אלחוטית בתוך {purchaseDaysRemaining} ימים. אנו נשלח שובר למסך הבית והודעה לחשבון הדואר האלקטרוני שלך ברגע שהזמנתך מעובד.השובר יכיל הוראות כיצד לממש את המבצע הזה.
 View All Special Offers	צפה בכל ההצעות המיוחדות
 View Special Offers	הצד הצעות מיוחדות
-We are unable to process your request. Please try again later.	
-You already have {count} order(s) pending for this special offer.	
-You have other orders pending. Click here to view	
-You have successfully cancelled your order for {dealTitle} from {merchantName}. Your credit card was not charged.	
-You must register your Kindle to make a purchase.	
-Your order will be placed when you connect to wireless.	
-Your purchase could not be completed. This offer has expired.	
-Your purchase could not be completed. We are experiencing technical difficulties. Please try again later.	
-Your purchase could not be completed. You have reached the maximum purchase quantity.	
-{customerName}, you have nearly finished.	
+We are unable to process your request. Please try again later.	אין באפשרותנו לעבד את בקשתך. אנא נסה שוב מאוחר יותר.
+You already have {count} order(s) pending for this special offer.	כבר יש {count} הזמנות הממתינות עבור הצעה מיוחדת זו.
+You have other orders pending. Click here to view	יש לך הזמנות אחרות התלויות ועומדות. לחץ כאן לצפייה
+You have successfully cancelled your order for {dealTitle} from {merchantName}. Your credit card was not charged.	בטלת בהצלחה את ההזמנה עבור {dealTitle} מ {merchantName}. כרטיס האשראי שלך לא יחויב.
+You must register your Kindle to make a purchase.	קינדל שלך חייבת להיות רשומה כדי לבצע רכישה.
+Your order will be placed when you connect to wireless.	ההזמנה שלך תתבצע בעת חיבור לאלחוטי.
+Your purchase could not be completed. This offer has expired.	הרכישה שלך לא ניתן להשלים. הצעה זו פגה.
+Your purchase could not be completed. We are experiencing technical difficulties. Please try again later.	הרכישה שלך לא ניתן להשלים. אנו נתקלים בקשיים טכניים. אנא נסה שוב מאוחר יותר.
+Your purchase could not be completed. You have reached the maximum purchase quantity.	הרכישה שלך לא ניתן להשלים. הגעת לכמות הרכישה המרבית.
+{customerName}, you have nearly finished.	{customerName}, כמעט סיימת.
