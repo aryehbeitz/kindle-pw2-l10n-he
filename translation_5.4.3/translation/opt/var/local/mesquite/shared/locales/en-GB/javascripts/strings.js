1fc560a5de3228e173b3d2ff391dfe7dff779440
Aryeh Beitz <aryehbeitz@gmail.com>
2014-06-15 04:04:05 +0300

    Translated

--- translation_5.4.3/translation/opt/var/local/mesquite/shared/locales/en-GB/javascripts/strings.js
+++ translation_5.4.3/translation/opt/var/local/mesquite/shared/locales/en-GB/javascripts/strings.js
@@ -1,14 +1,14 @@
 Cancel	בטל
 Close	סגור
-Connection Required	
+Connection Required	נדרש חיבור
 Edit	ערוך
-Menu	
-OK	
-Remove	
+Menu	תפריט
+OK	אישור
+Remove	הסר
 Screen Light	אור מסך
 Shop in Kindle Store	לקניות בחנות קינדל
 Turn Off Wireless	כבה אלחוטי
 Turn On Wireless	הפעל אלחוטי
-Unable to Connect	
-Your Kindle is currently unable to connect.<br><br>Please try again later.	
-strings	
+Unable to Connect	לא ניתן להתחבר
+Your Kindle is currently unable to connect.<br><br>Please try again later.	קינדל שלך הוא כרגע לא מצליח להתחבר. <br> <br> אנא נסה שוב מאוחר יותר.
+strings	strings
