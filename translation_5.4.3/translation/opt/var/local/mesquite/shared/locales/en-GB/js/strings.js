7e727c93303bcbc52e1d5adae45d0fdb3bacf5c6
Aryeh Beitz <aryehbeitz@gmail.com>
2014-06-15 04:07:24 +0300

    Translated

--- translation_5.4.3/translation/opt/var/local/mesquite/shared/locales/en-GB/js/strings.js
+++ translation_5.4.3/translation/opt/var/local/mesquite/shared/locales/en-GB/js/strings.js
@@ -1,16 +1,16 @@
 Cancel	בטל
 Close	סגור
-Connection Required	
+Connection Required	נדרש חיבור
 Edit	ערוך
-Lock Rotation	
-Menu	
-OK	
-Register	
-Remove	
+Lock Rotation	נעילת סיבוב
+Menu	תפריט
+OK	אישור
+Register	הרשם
+Remove	הסר
 Shop in Kindle Store	לקניות בחנות קינדל
-This action requires wireless to be turned on.	
+This action requires wireless to be turned on.	נדרשת אלחוטית מופעלת לפעולה זו.
 Turn Off Wireless	כבה אלחוטי
 Turn On Wireless	הפעל אלחוטי
-Unable to Connect	
-Unlock Rotation	
-Your Kindle is currently unable to connect.<br><br>Please try again later.	
+Unable to Connect	לא ניתן להתחבר
+Unlock Rotation	בטל נעילת סיבוב
+Your Kindle is currently unable to connect.<br><br>Please try again later.	קינדל שלך הוא כרגע לא מצליח להתחבר. <br> <br> אנא נסה שוב מאוחר יותר.
